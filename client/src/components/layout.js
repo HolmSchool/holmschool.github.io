1acbb09066f02ee5382b9ed4c019c8dc5bc5778c
olepvcxvx <ryanholm@gmail.com>
2023-06-09 12:13:04 -0500

    font, hamburger

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -14,6 +14,7 @@ const navItem = css`
   text-decoration: none;
   color: #96d7af;
   size: 1.5rem;
+  
 
   &:hover {
     text-decoration: underline;
@@ -23,6 +24,148 @@ const navItem = css`
   @supports (-webkit-text-stroke: 0.5px #5c8c72) {
     -webkit-text-stroke: 0.5px #5c8c72;
   }
+`
+
+const hamburger = css`
+// @import url(https://fonts.googleapis.com/css?family=Raleway);
+// h2 {
+//   vertical-align: center;
+//   text-align: center;
+// }
+
+html, body {
+  margin: 0;
+  height: 100%;
+  
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.top-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  // background-color: #00BAF0;
+  // background: linear-gradient(to left, #f46b45, #eea849);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  color: #96d7af;
+  height: 50px;
+  padding: 1em;
+}
+
+.menu {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu > li {
+  margin: 0 1rem;
+  overflow: hidden;
+  font-family: 'Philosopher', sans-serif;
+}
+
+.menu-button-container {
+  display: none;
+  height: 100%;
+  width: 30px;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#menu-toggle {
+  display: none;
+}
+
+.menu-button,
+.menu-button::before,
+.menu-button::after {
+  display: block;
+  background-color: #96d7af;
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  border-radius: 2px;
+  margin-right: 50px;
+}
+
+.menu-button::before {
+  content: '';
+  margin-top: -8px;
+}
+
+.menu-button::after {
+  content: '';
+  margin-top: 8px;
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button::before {
+  margin-top: 0px;
+  transform: rotate(405deg);
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button {
+  background: rgba(255, 255, 255, 0);
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button::after {
+  margin-top: 0px;
+  transform: rotate(-405deg);
+}
+
+@media (max-width: 700px) {
+  .menu-button-container {
+    display: flex;
+  }
+  .menu {
+    position: absolute;
+    top: 0;
+    margin-top: 50px;
+    left: 0;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+  #menu-toggle ~ .menu li {
+    height: 0;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  #menu-toggle:checked ~ .menu li {
+    border: 1px solid #333;
+    height: 2.5em;
+    padding: 0.5em;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  .menu > li {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0.5em 0;
+    width: 100%;
+    color: #96d7af;
+    background-color: #222;
+  }
+  .menu > li:not(:last-child) {
+    border-bottom: 1px solid #444;
+  }
+}
+View Compiled
+
+
+Resources1× 0.5× 0.25×Rerun
+
 `
 
 const svgNav = css`
@@ -51,7 +194,7 @@ const header = ({ children }) => (
     }
     render={data => (
 
-      <div css={siteWideCSS} className={`wrapper`}>
+      <div css={siteWideCSS} className={[`wrapper`, `philosopher`]} css={[hamburger]}>
         <Helmet defaultTitle={`Holm School | University in a Raspberry Pi`} titleTemplate={`%s | Holm School`}>
           <meta name="og:type" content="website" />
           <meta name="og:site_name" content="Holm School" />
@@ -90,7 +233,7 @@ const header = ({ children }) => (
                 <img src={logo} css={css`
                  width: 42px;
                  height: 42px;
-                 margin-top: 4px;
+                 margin-top: 0px;
                  margin-left: 0px;
                  margin-bottom: 0px;
                  margin-right: 27px;
@@ -107,6 +250,7 @@ const header = ({ children }) => (
                  transition: 150ms;
                  transition-timing-function: ease-in-out;
                  vertical-align: top;
+                 
                  display: none;
                 &:hover {
                    color: #667c7c;
@@ -143,32 +287,58 @@ const header = ({ children }) => (
              flex-direction: row;
              justify-content: flex-end;
            `}>
-            <Link
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <section class="top-nav">
+              {/* <div>
+                Logo Here
+              </div> */}
+              <input id="menu-toggle" type="checkbox" />
+              <label class='menu-button-container' for="menu-toggle">
+              <div class='menu-button'></div>
+            </label>
+              <ul class="menu">
+              <li><Link
                 to={`/`}
                 activeStyle={{ textDecoration: "underline" }}
                 css={[css`
               `, navItem]}
               >
                 Home
-            </Link>
+            </Link></li>
                   
-                  <Link
+                 <li><Link
                       to={`/learn/`}
                       activeStyle={{ textDecoration: "underline" }}
                       css={[css`
                     `, navItem]}
                     >
                       Curricula
-                  </Link>
-                  <Link
+                  </Link></li>
+                 <li><Link
                       to={`/z-degrees/`}
                       activeStyle={{ textDecoration: "underline" }}
                       css={[css`
                     `, navItem]}
                     >
                       Z-Degrees
-                  </Link>
-                {/* <Link
+                  </Link></li>
+                  <li><Link
+                  to={`/about/`}
+                  activeStyle={{ textDecoration: "underline" }}
+                  css={[css`
+                `, navItem]}
+                >
+                  About
+              </Link></li>
+                <li><Link
+                  to={`/faq/`}
+                  activeStyle={{ textDecoration: "underline" }}
+                  css={[css`
+                `, navItem]}
+                >
+                  FAQ
+              </Link></li>
+                <li><Link
                   to={`/guide/`}
 
                   css={[css`
@@ -177,7 +347,7 @@ const header = ({ children }) => (
                   activeStyle={{ textDecoration: "underline" }}
                 >
                   Guide
-              </Link> */}
+              </Link></li>
                 {/* <Link
                   to={`/the-open-source-fiduciary/`}
                   activeStyle={{ textDecoration: "underline" }}
@@ -195,38 +365,23 @@ const header = ({ children }) => (
                   Scholarship
               </Link> */}
               
-                {/* <Link
+                <li><Link
                   to={`/blog/`}
                   activeStyle={{ textDecoration: "underline" }}
                   css={[css`
               `, navItem]}
                 >
                   Blog
-             </Link> */}
-             {/* <Link
-                  to={`/about/`}
-                  activeStyle={{ textDecoration: "underline" }}
-                  css={[css`
-                `, navItem]}
-                >
-                  About
-              </Link> */}
-                {/* <Link
-                  to={`/faq/`}
-                  activeStyle={{ textDecoration: "underline" }}
-                  css={[css`
-                `, navItem]}
-                >
-                  FAQ
-              </Link> */}
-                <a
+             </Link></li>
+             
+                <li><a
                   href={`https://github.com/HolmSchool`}
                   css={[css`
               
                `, navItem]}
                 >
                   <svg css={svgNav} id="github-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
-                </a>
+                </a></li>
                 {/* <a
                   href={`https://twitter.com/holmschool`}
                   css={[css`
@@ -235,17 +390,25 @@ const header = ({ children }) => (
                 >
                   <svg css={svgNav} id="twitter-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z"/></svg>
                 </a> */}
+              </ul>
+            </section>
+                
+
+
+            
               </div>
             </div>
           </div>
         </div>
-        
-        {children}
+        <div className={`main-text`}>
+          {children}
+        </div>
         <div css={css`
            margin-top: 10px;
            max-width: 800px;
            margin: auto;
-         `}>
+           
+         `} className={`main-text`}>
           <a href="https://scholarship.law.duke.edu/cgi/viewcontent.cgi?article=1280&context=lcp"><img src={public_domain_small} alt="Public Domain" css={css`;margin-left: 20px; width:120px;margin-right: 30px;float:left;margin-bottom: 0px;`} /></a>
           <span>Located in Topeka, KS.</span><br />
 
